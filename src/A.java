885aae2d7aac8e58acf8b5ca04640790e017af47
manishbattu <31736555+manishbattu@users.noreply.github.com>
2017-09-07 14:42:14 +0000

    mno

---
+++ src/A.java
@@ -0,0 +1,14 @@
+public class A
+{
+	int i;
+	
+	public A(int y)
+	{
+		this.i=y;
+	}
+	public void method1()
+	{
+		System.out.println("this is super class method");
+		System.out.println("super class variable : "+i);
+	}
+}
